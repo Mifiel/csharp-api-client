1559590b241318e8695b337a070cc2cafc57d300
Juan Zavala <ja.zavala.aguilar@gmail.com>
2019-11-05 16:33:10 -0600

    allow add viewers and add  parameters send_email and send_invites

---
+++ MifielAPI/MifielAPI/Objects/Viewer.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace MifielAPI.Objects
+{
+    public class Viewer
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+}
